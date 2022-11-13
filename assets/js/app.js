c15ad469d6e1138d3b81784c72e40a14d04ff456
JonathanPegaz <pegazjonathan@gmail.com>
2022-11-13 21:31:26 +0100

    create base template

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
-
 import '../css/app.scss';
+import { Dropdown } from 'bootstrap';
 
+document.addEventListener('DOMContentLoaded', () => {
+    enableDropdowns();
+})
+
+const enableDropdowns = () => {
+    const dropdownElementList = document.querySelectorAll('.dropdown-toggle')
+    dropdownElementList.map(function (dropdownToggleEL) {
+        return new Dropdown(dropdownToggleEL)
+    });
+}
