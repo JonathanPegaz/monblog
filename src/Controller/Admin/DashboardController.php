06c49b84a94fb33203ca57743437825e836560a3
JonathanPegaz <pegazjonathan@gmail.com>
2022-11-19 16:13:10 +0100

    media system

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,6 +6,8 @@ use App\Entity\Article;
 use App\Entity\Category;
 use App\Entity\Comment;
 use App\Entity\Menu;
+use App\Entity\User;
+use App\Entity\Media;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
@@ -38,20 +40,36 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::LinkToRoute('Aller sur le site', 'fa fa-undo', 'app_home');
-        
-        yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
-            MenuItem::linkToCrud('Tous les articles', 'fas fa-newspaper', Article::class),
-            MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
-            MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class)
-        ]);
 
-        yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
-            MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class),
-            MenuItem::linkToCrud('Articles', 'fas fa-newspaper', Menu::class),
-            MenuItem::linkToCrud('Liens personnalisés', 'fas fa-link', Menu::class),
-            MenuItem::linkToCrud('Catégories', 'fab fa-delicious', Menu::class),
-        ]);
+        if($this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::subMenu('Menus', 'fas fa-list')->setSubItems([
+                MenuItem::linkToCrud('Pages', 'fas fa-file', Menu::class),
+                MenuItem::linkToCrud('Articles', 'fas fa-newspaper', Menu::class),
+                MenuItem::linkToCrud('Liens personnalisés', 'fas fa-link', Menu::class),
+                MenuItem::linkToCrud('Catégories', 'fab fa-delicious', Menu::class),
+            ]);
+        }        
 
-        yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
+        if($this->isGranted('ROLE_AUTHOR')) {
+            yield MenuItem::subMenu('Articles', 'fas fa-newspaper')->setSubItems([
+                MenuItem::linkToCrud('Tous les articles', 'fas fa-newspaper', Article::class),
+                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Article::class)->setAction(Crud::PAGE_NEW),
+                MenuItem::linkToCrud('Catégories', 'fas fa-list', Category::class)
+            ]);
+
+            yield MenuItem::subMenu('Médias', 'fas fa-photo-video')->setSubItems([
+                MenuItem::linkToCrud('Médiathèque', 'fas fa-photo-video', Media::class),
+                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', Media::class)->setAction(Crud::PAGE_NEW)
+            ]);
+        }
+
+        if($this->isGranted('ROLE_ADMIN')) {
+            yield MenuItem::linkToCrud('Commentaires', 'fas fa-comment', Comment::class);
+
+            yield MenuItem::subMenu('Comptes', 'fas fa-user')->setSubItems([
+                MenuItem::linkToCrud('Tous les comptes', 'fas fa-user-friends', User::class),
+                MenuItem::linkToCrud('Ajouter', 'fas fa-plus', User::class)->setAction(Crud::PAGE_NEW)
+            ]);
+        }
     }
 }
